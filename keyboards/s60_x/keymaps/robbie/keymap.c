ee366a5d39a90a81ac1ddd563cd1ab5ff2e97900
robjloranger <robjloranger@protonmail.com>
2018-06-28 17:07:52 -0700

    forgot to swap left ctrl/alt

--- keyboards/s60_x/keymaps/robbie/keymap.c
+++ keyboards/s60_x/keymaps/robbie/keymap.c
@@ -10,7 +10,7 @@
 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
 │LSHFT│▒▒▒▒▒│  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │▒▒▒▒▒│RSHFT│▒▒▒▒▒│
 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-│LCTRL│L_GUI│L_ALT│█████│█████│█████│ SPC │█████│█████│█████│RCTRL│ FN0 │ APP │R_ALT│█████│
+│L_ALT│L_GUI│LCTRL│█████│█████│█████│ SPC │█████│█████│█████│RCTRL│ FN0 │ APP │R_ALT│█████│
 └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
 */
 
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,       \
           KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NO,   KC_ENT ,       \
           KC_LSFT,   KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT, KC_NO, \
-          KC_LCTL,   KC_LGUI, KC_LALT,                   KC_SPC,                    KC_RCTL, KC_FN0,  KC_APP,  KC_RALT),
+          KC_LALT,   KC_LGUI, KC_LCTL,                   KC_SPC,                    KC_RCTL, KC_FN0,  KC_APP,  KC_RALT),
 
 /* 1: Fn layer
 UG = Underglow = RGB Backlighting
